5b79fb86f04093813c6b22b22a560d835a10b05d
Charles Harvey <harvey.charles@gmail.com>
2019-02-18 21:05:36 +0100

    remove copyright footer for now

--- footer.php
+++ footer.php
@@ -1,39 +1,41 @@
-<?php $td = get_template_directory_uri(); ?>
+<?php $tdu = get_template_directory_uri(); ?>
 
 
 <!-- footer -->
 <footer class="footer" role="contentinfo">
 
-
+<?php if (false): ?>
 	<!-- copyright -->
 	<div class="container-fluid copyright">
 	<div class="row">
 		<div class="col-sm-4 col-md-2">
 			<h3></h3>
-			<a href="http://www.transcontinental.ch/" target="_blank"><img src="<?php echo $td; ?>/img/trans.png" alt="" /></a>
+			<a href="http://www.transcontinental.ch/" target="_blank"><img src="<?php echo $tdu; ?>/img/trans.png" alt="" /></a>
 		</div>
 		<div class="col-sm-4 col-md-5">
 			<h3>Autres sociétés membres du groupe Transcontinental</h3>
-			<a href="http://www.zenithvoyages.ch/" target="_blank"><img src="<?php echo $td; ?>/img/zenith.svg" alt="" /></a>
-			<a href="http://www.gva-vip.ch/" target="_blank"><img style="background:#353537; padding: 5px;" src="<?php echo $td; ?>/img/logo_gva.png" alt="" /></a>
-			<a href="http://gtmgolf.ch/" target="_blank"><img style="padding: 0; margin-left: 10px;" src="<?php echo $td; ?>/img/gtm.jpg" alt="" /></a>
+			<a href="http://www.zenithvoyages.ch/" target="_blank"><img src="<?php echo $tdu; ?>/img/zenith.svg" alt="" /></a>
+			<a href="http://www.gva-vip.ch/" target="_blank"><img style="background:#353537; padding: 5px;" src="<?php echo $tdu; ?>/img/logo_gva.png" alt="" /></a>
+			<a href="http://gtmgolf.ch/" target="_blank"><img style="padding: 0; margin-left: 10px;" src="<?php echo $tdu; ?>/img/gtm.jpg" alt="" /></a>
 		</div>
 		<div class="col-sm-4 col-md-5">
 			<h3>Affiliations professionnelles</h3>
-			<a href="http://www.garantiefonds.ch/fr/page-daccueil/" target="_blank"><img src="<?php echo $td; ?>/img/garantie.jpg" alt="" /></a>
-			<a href="http://www.iata.org/Pages/default.aspx" target="_blank"><img src="<?php echo $td; ?>/img/iata.png" alt="" /></a>
-			<a href="http://www.srv.ch/fr/page-daccueil/" target="_blank"><img src="<?php echo $td; ?>/img/fsv.png" alt="" /></a>
-		<a href="http://itptravel.com/" target="_blank"><img src="<?php echo $td; ?>/img/itp.png" alt="" /></a>
+			<a href="http://www.garantiefonds.ch/fr/page-daccueil/" target="_blank"><img src="<?php echo $tdu; ?>/img/garantie.jpg" alt="" /></a>
+			<a href="http://www.iata.org/Pages/default.aspx" target="_blank"><img src="<?php echo $tdu; ?>/img/iata.png" alt="" /></a>
+			<a href="http://www.srv.ch/fr/page-daccueil/" target="_blank"><img src="<?php echo $tdu; ?>/img/fsv.png" alt="" /></a>
+		<a href="http://itptravel.com/" target="_blank"><img src="<?php echo $tdu; ?>/img/itp.png" alt="" /></a>
 		</div>
 	</div>
-<!-- 		<a href="http://www.iata.org/Pages/default.aspx" target="_blank"><img src="<?php echo $td; ?>/img/iata.png" alt="" /></a> -->
-		<!-- <a href="http://www.srv.ch/fr/page-daccueil/" target="_blank"><img src="<?php echo $td; ?>/img/fsdadv.png" alt="" /></a> -->
-<!-- 		<a href="http://www.srv.ch/fr/page-daccueil/" target="_blank"><img src="<?php echo $td; ?>/img/fsv.png" alt="" /></a>
-		<a href="http://itptravel.com/" target="_blank"><img src="<?php echo $td; ?>/img/itp.png" alt="" /></a>
-		<a href="http://www.garantiefonds.ch/fr/page-daccueil/" target="_blank"><img src="<?php echo $td; ?>/img/garantie.jpg" alt="" /></a>
-		<a href="http://www.transcontinental.ch/" target="_blank"><img src="<?php echo $td; ?>/img/trans.png" alt="" /></a>
-		<a href="http://www.zenithvoyages.ch/" target="_blank"><img src="<?php echo $td; ?>/img/zenith.svg" alt="" /></a> -->
+<!-- 		<a href="http://www.iata.org/Pages/default.aspx" target="_blank"><img src="<?php echo $tdu; ?>/img/iata.png" alt="" /></a> -->
+		<!-- <a href="http://www.srv.ch/fr/page-daccueil/" target="_blank"><img src="<?php echo $tdu; ?>/img/fsdadv.png" alt="" /></a> -->
+<!-- 		<a href="http://www.srv.ch/fr/page-daccueil/" target="_blank"><img src="<?php echo $tdu; ?>/img/fsv.png" alt="" /></a>
+		<a href="http://itptravel.com/" target="_blank"><img src="<?php echo $tdu; ?>/img/itp.png" alt="" /></a>
+		<a href="http://www.garantiefonds.ch/fr/page-daccueil/" target="_blank"><img src="<?php echo $tdu; ?>/img/garantie.jpg" alt="" /></a>
+		<a href="http://www.transcontinental.ch/" target="_blank"><img src="<?php echo $tdu; ?>/img/trans.png" alt="" /></a>
+		<a href="http://www.zenithvoyages.ch/" target="_blank"><img src="<?php echo $tdu; ?>/img/zenith.svg" alt="" /></a> -->
 	</div>
+
+<?php endif; // end if false ?>
 
 	<div class="footer_inner">
 	<div class="container">
@@ -69,14 +71,14 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>
 <?php wp_footer(); ?>
 
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/unslider-min.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.matchHeight-min.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.shuffle.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/featherlight.min.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/featherlight.gallery.min.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.justifiedGallery.min.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/slick.min.js"></script>
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/scripts.js?v=0.2"></script>
+<script type="text/javascript" src="<?php echo $tdu; ?>/js/unslider-min.js"></script>
+<script type="text/javascript" src="<?php echo $tdu; ?>/js/jquery.matchHeight-min.js"></script>
+<script type="text/javascript" src="<?php echo $tdu; ?>/js/jquery.shuffle.js"></script>
+<script type="text/javascript" src="<?php echo $tdu; ?>/js/featherlight.min.js"></script>
+<script type="text/javascript" src="<?php echo $tdu; ?>/js/featherlight.gallery.min.js"></script>
+<script type="text/javascript" src="<?php echo $tdu; ?>/js/jquery.justifiedGallery.min.js"></script>
+<script type="text/javascript" src="<?php echo $tdu; ?>/js/slick.min.js"></script>
+<script type="text/javascript" src="<?php echo $tdu; ?>/js/scripts.js?v=0.2"></script>
 <!-- analytics -->
 <script>
 	// (function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
