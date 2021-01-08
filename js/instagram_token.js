62044633412ea13a4d8c71f502e06daa9451649c
Charles Harvey <harvey.charles@gmail.com>
2021-01-08 18:15:47 +0000

    added  new instr token

--- js/instagram_token.js
+++ js/instagram_token.js
@@ -1,1 +1,1 @@
-var instagram_token = 'null'
+var instagram_token = 'AQBfw-o5A8_g6qHlHnoY56QDBgHkZ0ExbFND3FvGk2fFdlxzizdYiK9WDLFxUMsFCa1xaLaxyIL-2R4pu391KDk9tBI0vZgSRbbrTckipUx-1VMTa7jsl1aR5VV7C00WTvR_nvLa2VtEDzGLyxMfBe35vszB-jigYZn9H2jP7jU4nWoHoLvkw5yLbZDlffot07wJLQ5jHRgIFzoFc8XSrCL93K06ShzqkuxxHsiBCL2X4w'
