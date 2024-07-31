59b51e46125d3c6d5be86504bfaa4098ef0f71b5
Charles Harvey <harvey.charles@gmail.com>
2024-07-31 10:56:18 +0100

    new google map js

--- js/scripts.js
+++ js/scripts.js
@@ -1,432 +1,376 @@
 $(function () {
-
-
-	$(".header_slider").slick({
-		infinite: true,
-		autoplay: true,
-		slidesToShow: 1,
-		dots: false,
-		arrows: false,
-		fade: true,
-		speed: 1000,
-		autoplaySpeed: 3000
-
-	});
-
-	//Offers slider
-	$(".home #offers_slider").slick({
-
-		// normal options...
-		infinite: true,
-		autoplay: false,
-		dots: false,
-		slidesToShow: 3,
-
-		// the magic
-		responsive: [{
-
-			breakpoint: 992,
-			settings: {
-				slidesToShow: 2,
-			}
-
-		}, {
-
-			breakpoint: 568,
-			settings: {
-				slidesToShow: 1
-			}
-
-		}]
-	});
-
-	$("#gallery_slider").slick({
-
-		// normal options...
-		infinite: true,
-		adaptiveHeight: true,
-		autoplay: true,
-		speed: 500,
-		dots: false,
-		slidesToShow: 1,
-		fade: true,
-		cssEase: 'linear'
-	});
-
-	$('a#offre_slide').on('click', function (e) {
-		$offset = $('header').height() + 50;
-		console.log($offset);
-		$("html, body").animate({ scrollTop: $('#reserver').offset().top - $offset }, 1000);
-	});
-
-
-
-	$(".img_gallery").justifiedGallery({
-		rowHeight: 150,
-		margins: 4,
-		lastRow: 'justify'
-	}).on('jg.complete', function () {
-		$('a.gallery').featherlightGallery({
-			previousIcon: '<',
-			nextIcon: '>'
-		});
-
-	});
-
-
-	// 	$(".img_gallery").justifiedGallery({
-	// 	 rowHeight : 150,
-	// 	 margins: 4,
-	// 	 lastRow : 'justify'
-	// }).on('jg.complete', function () {
-	//     $(this).find('a').colorbox({
-	//         maxWidth : '80%',
-	//         maxHeight : '80%',
-	//         overlayClose : true,
-	//         opacity : 0.8,
-	//         transition : 'elastic',
-	//         current : ''
-	//     });
-
-
-	// });
-
-	var $nav = $('#nav');
-
-
-	var unslider = $('.slideshow').unslider({
-		'animation': 'fade',
-		'nav': false,
-		'arrows': true,
-		'autoplay': true,
-		'delay': 6000
-	});
-
-
-	unslider.on('unslider.change', function (event, index, slide) {
-
-		if (typeof map !== 'undefined') {
-			// MOVE MAP CENTER TO NEW SLIDE LOCATION
-			// var $lat_lon = $('#lat_lon');
-			// var $lat_lon_current = slide[0].dataset.latlon;
-			// $lat_lon.val(  $lat_lon_current );
-			// console.log('current', $lat_lon_current);
-		}
-
-	});
-
-	var $window = $(window);
-	var $body = $('body');
-	var $windowHeight = $window.height();
-	var $header = $('header');
-	var $social_bar = $('#social_bar');
-
-
-	$window.scroll(function () {
-
-		var windowScroll = $window.scrollTop();
-
-
-		if (windowScroll > (300)) {
-			$header.addClass('visible_header');
-			$social_bar.addClass('visible_bar');
-		} else {
-			$header.removeClass('visible_header');
-			$social_bar.removeClass('visible_bar');
-		}
-
-
-
-	});
-
-
-	$('.offre_content .allbutlink').matchHeight();
-	$('.reservation_box .allbutlink').matchHeight();
-	$('.block h4').matchHeight();
-
-	$('.iframe_link').on('click', function (e) {
-		e.preventDefault();
-		var $this = $(this);
-
-		var $placetoslide = $('#placetoslide');
-
-		loadIframe($this);
-
-		$("html, body").animate({ scrollTop: ($placetoslide.offset().top - 100) }, 2000);
-
-	});
-
-
-	function loadIframe($element) {
-		var $this = $element;
-		var $url = $this.data('url');
-		var $message = $this.data('message');
-		var $piframe = $('#page_iframe');
-		var $message_to_show = $('#message_to_show');
-		$message_to_show.html('');
-
-		if (typeof $url != 'undefined') {
-			$piframe.attr({ 'src': $url });
-			setTimeout(function () {
-				$piframe.css({ 'height': 700 });
-			}, 1000);
-
-
-
-		} else if (typeof $message != 'undefined') {
-			var $piframe = $('#page_iframe').css({ 'height': 0 });
-			$message_to_show.html($message);
-		}
-
-	}
-
-	$firstiframelink = $('.offre_iframe .iframe_link').first();
-	loadIframe($firstiframelink);
-
-
-	function iframeresize() {
-		var $iframeheight = $('.iframecontained').height();
-		$('.iframecontainer').css({ 'height': $iframeheight });
-	}
-	iframeresize();
-
-
-	$('.sidebar_inner').each(function () {
-		$this = $(this);
-		console.log($this.find('li').size());
-		if ($this.find('li').size() < 2) {
-			$this.hide();
-		};
-	})
-
-
-	$('aside, #main_article').matchHeight();
-	$('.iframe_links a').matchHeight();
-
-	$('.matchme').matchHeight();
-
-	$send_offer_form = $('#send_offer');
-	if (typeof send_offer_url !== 'undefined') {
-
-		$send_offer_form.submit(function (e) {
-
-			e.preventDefault();
-
-
-			var $form = $(this);
-			var $name = $('#name');
-			var $email = $('#email');
-			var $message = $('#message');
-			var $number = $('#number');
-			var $offer_title = $('#offer_title');
-
-			$errors = [];
-
-			if ($email.val() == '') {
-				$errors.push("Please enter an email address");
-			}
-			else if ($email.val().indexOf("@") < 0) {
-				$errors.push("Your email address is not valid.");
-			}
-			if ($message.val() == '') {
-				$errors.push("Please enter a message");
-			}
-
-			if ($errors.length > 0) {
-				$('#sendOfferResponse').html($errors.join('<br/>')).addClass('warning').removeClass('success');
-			} else {  // EMAIL IS GOOD SEND
-
-
-
-				$.ajax({
-					url: send_offer_url,
-					type: "POST",
-					data: {
-						name: $name.val(),
-						email: $email.val(),
-						message: $message.val(),
-						number: $number.val(),
-						offer_title: $offer_title.val()
-
-					},
-					success: function (data) {
-						$('#sendOfferResponse').html(data).addClass('success').removeClass('warning');
-						$('input[type="text"], input[type="email"], textarea').val('');
-					},
-					error: function (data) {
-						$('#sendOfferResponse').html(data).addClass('warning').removeClass('success');
-
-					}
-				});
-				// UPDATE MESSAGE
-
-
-			}
-
-		}); // end of $send_offer_form
-	} // if typeof send_offer_url
-
-
-
-
-
-	$('#show_nav_button').on('click', function () {
-		$nav.find('ul.menu').toggle();
-	});
-	$('.menu-item-has-children').on('click', function () {
-		$(this).children('ul.sub-menu').toggle();
-		$(this).toggleClass('activenavli');
-	});
-
-
-	$('li', $nav).on('mouseover', function () {
-		$this = $(this);
-		$this.find('ul').addClass('ul_child_show');
-	}).on('mouseout', function () {
-		$this.find('ul').removeClass('ul_child_show');
-	});
-
-
-
-	$('.map_location').on('mouseover', function () {
-
-		var $lat_lon = $('.unslider-active').data('latlon').split(',');
-		var newLatlng = { lat: parseFloat($lat_lon[0]), lng: parseFloat($lat_lon[1]) };
-		map.setCenter(newLatlng);
-
-	});
-
-	$('.map_location').featherlight('#mylightbox', {
-		beforeOpen: function () {
-
-
-			// resize if window resized
-			$window = $(window);
-			mapcontainer.css({ width: $window.width() * 0.75 });
-			google.maps.event.trigger(map, 'resize');
-
-
-
-
-		}
-	});
-
-
-
-	var mapcontainer = $('#googleMap');
-	var myMapOptions = {
-		zoom: 15,
-		mapTypeControl: false,
-		scrollwheel: true
-	};
-
-
-
-	if (mapcontainer.length > 0) {
-		// var $lat_lon = $('.unslider-active').data('latlon').split(',');
-		//    var myLatlng = {lat: parseFloat($lat_lon[0]) , lng: parseFloat($lat_lon[1]) };
-		var map = new google.maps.Map(mapcontainer.get(0), myMapOptions);
-		var $window = $(window);
-		mapcontainer.css({
-			width: $window.width() * 0.75,
-			height: 370
-		})
-
-		//	 map.setCenter( myLatlng   );
-
-	}
-
-
-
-
-	// instagram
-	if (typeof instagram_token !== 'undefined') {
-		if ($('#instafeed').length) {
-			var feed = new Instafeed({
-				get: 'user',
-				clientId: 'd37968ca616b48de9d142b1a33bee2fa',
-				userId: 10715708705,
-				accessToken: instagram_token,
-				sortBy: 'most-recent',
-				limit: 6,
-				resolution: 'standard_resolution',
-				template: '<a target="_blank" href="{{link}}" style="background-image:url({{image}})"></a>',
-				success: function (data) {
-				}
-			});
-			feed.run();
-		}
-	} else {
-		$('.instafeed_container').hide();
-	}
-
-
-
-	/// CONTACT ME AGENTS SLIDER
-	$('.agent_section').hide();
-	$('.agent_section:eq(0)').show();
-	$agent_links = $('.agent_link');
-	$agent_links.first().addClass('selected');
-
-	$agent_links.on('click', function (e) {
-		$this = $(this);
-		$id = $this.data('id')
-		$('.agent_section').hide();
-		$('#' + $id).show();
-		$agent_links.removeClass('selected');
-		$this.addClass('selected');
-		$('aside, #main_article').matchHeight();
-
-
-	});
-
-
-	if (typeof locations != 'undefined') {
-
-		var map_container = $('#agencymap');
-		map_container.css({
-			width: '100%'
-		})
-		var agencymap = new google.maps.Map(map_container.get(0), {
-			// center: {lat: latitude , lng: longitude  },
-			zoom: 14,
-			scrollwheel: false
-		});
-
-		var marker, i;
-		//location is set in php in template-home.php
-		var bounds = new google.maps.LatLngBounds();
-		var infowindow = new google.maps.InfoWindow({ content: '...' });
-		for (i = 0; i < locations.length; i++) {
-			var location = locations[i];
-			var latlng = new google.maps.LatLng(location[0], location[1]),
-				marker = new google.maps.Marker({
-					position: latlng,
-					map: agencymap,
-					title: location[2]
-				});
-			marker.addListener('click', function () {
-				infowindow.setContent(this.title);
-				infowindow.open(agencymap, this);
-			});
-			bounds.extend(latlng);
-
-			if (locations.length == 1) {
-				agencymap.setZoom(13);
-				agencymap.setCenter(latlng);
-			}
-
-
-		}
-
-		if (locations.length > 1) {
-			agencymap.fitBounds(bounds);
-		}
-
-
-
-
-	}  // end of if locations defined
-
-
+  $(".header_slider").slick({
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 1,
+    dots: false,
+    arrows: false,
+    fade: true,
+    speed: 1000,
+    autoplaySpeed: 3000,
+  });
+
+  //Offers slider
+  $(".home #offers_slider").slick({
+    // normal options...
+    infinite: true,
+    autoplay: false,
+    dots: false,
+    slidesToShow: 3,
+
+    // the magic
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 568,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  $("#gallery_slider").slick({
+    // normal options...
+    infinite: true,
+    adaptiveHeight: true,
+    autoplay: true,
+    speed: 500,
+    dots: false,
+    slidesToShow: 1,
+    fade: true,
+    cssEase: "linear",
+  });
+
+  $("a#offre_slide").on("click", function (e) {
+    $offset = $("header").height() + 50;
+    console.log($offset);
+    $("html, body").animate(
+      { scrollTop: $("#reserver").offset().top - $offset },
+      1000
+    );
+  });
+
+  $(".img_gallery")
+    .justifiedGallery({
+      rowHeight: 150,
+      margins: 4,
+      lastRow: "justify",
+    })
+    .on("jg.complete", function () {
+      $("a.gallery").featherlightGallery({
+        previousIcon: "<",
+        nextIcon: ">",
+      });
+    });
+
+  // 	$(".img_gallery").justifiedGallery({
+  // 	 rowHeight : 150,
+  // 	 margins: 4,
+  // 	 lastRow : 'justify'
+  // }).on('jg.complete', function () {
+  //     $(this).find('a').colorbox({
+  //         maxWidth : '80%',
+  //         maxHeight : '80%',
+  //         overlayClose : true,
+  //         opacity : 0.8,
+  //         transition : 'elastic',
+  //         current : ''
+  //     });
+
+  // });
+
+  var $nav = $("#nav");
+
+  var unslider = $(".slideshow").unslider({
+    animation: "fade",
+    nav: false,
+    arrows: true,
+    autoplay: true,
+    delay: 6000,
+  });
+
+  unslider.on("unslider.change", function (event, index, slide) {
+    if (typeof map !== "undefined") {
+      // MOVE MAP CENTER TO NEW SLIDE LOCATION
+      // var $lat_lon = $('#lat_lon');
+      // var $lat_lon_current = slide[0].dataset.latlon;
+      // $lat_lon.val(  $lat_lon_current );
+      // console.log('current', $lat_lon_current);
+    }
+  });
+
+  var $window = $(window);
+  var $body = $("body");
+  var $windowHeight = $window.height();
+  var $header = $("header");
+  var $social_bar = $("#social_bar");
+
+  $window.scroll(function () {
+    var windowScroll = $window.scrollTop();
+
+    if (windowScroll > 300) {
+      $header.addClass("visible_header");
+      $social_bar.addClass("visible_bar");
+    } else {
+      $header.removeClass("visible_header");
+      $social_bar.removeClass("visible_bar");
+    }
+  });
+
+  $(".offre_content .allbutlink").matchHeight();
+  $(".reservation_box .allbutlink").matchHeight();
+  $(".block h4").matchHeight();
+
+  $(".iframe_link").on("click", function (e) {
+    e.preventDefault();
+    var $this = $(this);
+
+    var $placetoslide = $("#placetoslide");
+
+    loadIframe($this);
+
+    $("html, body").animate(
+      { scrollTop: $placetoslide.offset().top - 100 },
+      2000
+    );
+  });
+
+  function loadIframe($element) {
+    var $this = $element;
+    var $url = $this.data("url");
+    var $message = $this.data("message");
+    var $piframe = $("#page_iframe");
+    var $message_to_show = $("#message_to_show");
+    $message_to_show.html("");
+
+    if (typeof $url != "undefined") {
+      $piframe.attr({ src: $url });
+      setTimeout(function () {
+        $piframe.css({ height: 700 });
+      }, 1000);
+    } else if (typeof $message != "undefined") {
+      var $piframe = $("#page_iframe").css({ height: 0 });
+      $message_to_show.html($message);
+    }
+  }
+
+  $firstiframelink = $(".offre_iframe .iframe_link").first();
+  loadIframe($firstiframelink);
+
+  function iframeresize() {
+    var $iframeheight = $(".iframecontained").height();
+    $(".iframecontainer").css({ height: $iframeheight });
+  }
+  iframeresize();
+
+  $(".sidebar_inner").each(function () {
+    $this = $(this);
+    console.log($this.find("li").size());
+    if ($this.find("li").size() < 2) {
+      $this.hide();
+    }
+  });
+
+  $("aside, #main_article").matchHeight();
+  $(".iframe_links a").matchHeight();
+
+  $(".matchme").matchHeight();
+
+  $send_offer_form = $("#send_offer");
+  if (typeof send_offer_url !== "undefined") {
+    $send_offer_form.submit(function (e) {
+      e.preventDefault();
+
+      var $form = $(this);
+      var $name = $("#name");
+      var $email = $("#email");
+      var $message = $("#message");
+      var $number = $("#number");
+      var $offer_title = $("#offer_title");
+
+      $errors = [];
+
+      if ($email.val() == "") {
+        $errors.push("Please enter an email address");
+      } else if ($email.val().indexOf("@") < 0) {
+        $errors.push("Your email address is not valid.");
+      }
+      if ($message.val() == "") {
+        $errors.push("Please enter a message");
+      }
+
+      if ($errors.length > 0) {
+        $("#sendOfferResponse")
+          .html($errors.join("<br/>"))
+          .addClass("warning")
+          .removeClass("success");
+      } else {
+        // EMAIL IS GOOD SEND
+
+        $.ajax({
+          url: send_offer_url,
+          type: "POST",
+          data: {
+            name: $name.val(),
+            email: $email.val(),
+            message: $message.val(),
+            number: $number.val(),
+            offer_title: $offer_title.val(),
+          },
+          success: function (data) {
+            $("#sendOfferResponse")
+              .html(data)
+              .addClass("success")
+              .removeClass("warning");
+            $('input[type="text"], input[type="email"], textarea').val("");
+          },
+          error: function (data) {
+            $("#sendOfferResponse")
+              .html(data)
+              .addClass("warning")
+              .removeClass("success");
+          },
+        });
+        // UPDATE MESSAGE
+      }
+    }); // end of $send_offer_form
+  } // if typeof send_offer_url
+
+  $("#show_nav_button").on("click", function () {
+    $nav.find("ul.menu").toggle();
+  });
+  $(".menu-item-has-children").on("click", function () {
+    $(this).children("ul.sub-menu").toggle();
+    $(this).toggleClass("activenavli");
+  });
+
+  $("li", $nav)
+    .on("mouseover", function () {
+      $this = $(this);
+      $this.find("ul").addClass("ul_child_show");
+    })
+    .on("mouseout", function () {
+      $this.find("ul").removeClass("ul_child_show");
+    });
+
+  $(".map_location").on("mouseover", function () {
+    var $lat_lon = $(".unslider-active").data("latlon").split(",");
+    var newLatlng = {
+      lat: parseFloat($lat_lon[0]),
+      lng: parseFloat($lat_lon[1]),
+    };
+    map.setCenter(newLatlng);
+  });
+
+  $(".map_location").featherlight("#mylightbox", {
+    beforeOpen: function () {
+      // resize if window resized
+      $window = $(window);
+      mapcontainer.css({ width: $window.width() * 0.75 });
+      google.maps.event.trigger(map, "resize");
+    },
+  });
+
+  var mapcontainer = $("#googleMap");
+  var myMapOptions = {
+    zoom: 15,
+    mapTypeControl: false,
+    scrollwheel: true,
+  };
+
+  if (mapcontainer.length > 0) {
+    // var $lat_lon = $('.unslider-active').data('latlon').split(',');
+    //    var myLatlng = {lat: parseFloat($lat_lon[0]) , lng: parseFloat($lat_lon[1]) };
+    var map = new google.maps.Map(mapcontainer.get(0), myMapOptions);
+    var $window = $(window);
+    mapcontainer.css({
+      width: $window.width() * 0.75,
+      height: 370,
+    });
+
+    //	 map.setCenter( myLatlng   );
+  }
+
+  // instagram
+  if (typeof instagram_token !== "undefined") {
+    if ($("#instafeed").length) {
+      var feed = new Instafeed({
+        get: "user",
+        clientId: "d37968ca616b48de9d142b1a33bee2fa",
+        userId: 10715708705,
+        accessToken: instagram_token,
+        sortBy: "most-recent",
+        limit: 6,
+        resolution: "standard_resolution",
+        template:
+          '<a target="_blank" href="{{link}}" style="background-image:url({{image}})"></a>',
+        success: function (data) {},
+      });
+      feed.run();
+    }
+  } else {
+    $(".instafeed_container").hide();
+  }
+
+  /// CONTACT ME AGENTS SLIDER
+  $(".agent_section").hide();
+  $(".agent_section:eq(0)").show();
+  $agent_links = $(".agent_link");
+  $agent_links.first().addClass("selected");
+
+  $agent_links.on("click", function (e) {
+    $this = $(this);
+    $id = $this.data("id");
+    $(".agent_section").hide();
+    $("#" + $id).show();
+    $agent_links.removeClass("selected");
+    $this.addClass("selected");
+    $("aside, #main_article").matchHeight();
+  });
+
+  if (typeof locations != "undefined") {
+    var map_container = $("#agencymap");
+    map_container.css({
+      width: "100%",
+    });
+    var agencymap = new google.maps.Map(map_container.get(0), {
+      // center: {lat: latitude , lng: longitude  },
+      zoom: 14,
+      scrollwheel: false,
+    });
+
+    var marker, i;
+    //location is set in php in template-home.php
+    var bounds = new google.maps.LatLngBounds();
+    var infowindow = new google.maps.InfoWindow({ content: "..." });
+    for (i = 0; i < locations.length; i++) {
+      var location = locations[i];
+      var latlng = new google.maps.LatLng(location[0], location[1]),
+        marker = new google.maps.Marker({
+          position: latlng,
+          map: agencymap,
+          title: location[2],
+        });
+      marker.addListener("click", function () {
+        infowindow.setContent(this.title);
+        infowindow.open(agencymap, this);
+      });
+      bounds.extend(latlng);
+
+      if (locations.length == 1) {
+        agencymap.setZoom(13);
+        agencymap.setCenter(latlng);
+      }
+    }
+
+    if (locations.length > 1) {
+      agencymap.fitBounds(bounds);
+    }
+  } // end of if locations defined
 });
--- template-parent.php
+++ template-parent.php
@@ -1,10 +1,10 @@
 <?php /* Template Name: Page parente */ get_header(); ?>
 
-	<?php if ( has_post_thumbnail()) :  ?>
+<?php if (has_post_thumbnail()) :  ?>
 	<div class="page_image" style="background-image:url('<?php echo the_post_thumbnail_url(); ?>')"></div>
-	<?php else: ?>
-		<div id="featured_slide"><?php  get_template_part('slides'); ?></div>
-	<?php endif; ?>
+<?php else : ?>
+	<div id="featured_slide"><?php get_template_part('slides'); ?></div>
+<?php endif; ?>
 
 <div class="page_title">
 	<h1 class="container"><?php the_title(); ?></h1>
@@ -14,115 +14,120 @@
 
 
 
-<div class="container" >
-<div class="container" >
-	<div class="row">
+<div class="container">
+	<div class="container">
+		<div class="row">
 
-		<!-- section -->
-		<!-- <section id="main_col" class="col-sm-9 "> -->
-		<section >
+			<!-- section -->
+			<!-- <section id="main_col" class="col-sm-9 "> -->
+			<section>
 
-			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-				<!-- article -->
-				<article id="main_article" <?php post_class(); ?>>
+						<!-- article -->
+						<article id="main_article" <?php post_class(); ?>>
 
-				<?php the_content(); ?>
-				<hr>
-				<br>
-				<br>
+							<?php the_content(); ?>
+							<hr>
+							<br>
+							<br>
 
 
 
-			<?php $children = get_posts( array(  'posts_per_page' => -1, 'post_type'=>'page',  'post_parent' =>  $post->ID )  ); ?>
-			<?php if (sizeof($children) > 0 ): ?>
-			<?php $i=1; ?>
-			<div class="row">
-				<?php foreach ($children as $child) : ?>
-					<?php $child_id = $child->ID; ?>
-					<div class="col-sm-4">
-						<a href="<?php echo $child->guid; ?>">
-							<div class="block" style="background-image: url(<?php echo get_the_post_thumbnail_url($child_id); ?>);">
-								<h4><?php print_r($child->post_title); ?></h4>
+							<?php $children = get_posts(array('posts_per_page' => -1, 'post_type' => 'page',  'post_parent' =>  $post->ID)); ?>
+							<?php if (sizeof($children) > 0) : ?>
+								<?php $i = 1; ?>
+								<div class="row">
+									<?php foreach ($children as $child) : ?>
+										<?php $child_id = $child->ID; ?>
+										<div class="col-sm-4">
+											<a href="<?php echo $child->guid; ?>">
+												<div class="block" style="background-image: url(<?php echo get_the_post_thumbnail_url($child_id); ?>);">
+													<h4><?php print_r($child->post_title); ?></h4>
+												</div>
+											</a>
+										</div>
+										<?php if ($i % 3 == 0) {
+											echo '</div><div class="row">';
+										} ?>
+										<?php $i++; ?>
+									<?php endforeach; ?>
+								</div>
+							<?php endif; ?>
+							<hr>
+							<br>
+
+							<h3>Contactez-nous</h3>
+
+
+							<div class="row">
+								<div class="col-sm-4">
+									<p>
+										TRANSCONTINENTAL Florissant<br>
+										66, route de Florissant<br>
+										CH – 1206 Genève<br>
+										T +41 22 347 27 27<br>
+										<a href="mailto:info@transcontinental.ch">info@transcontinental.ch</a>
+									</p>
+								</div>
+								<div class="col-sm-4">
+									<p>
+										TRANSCONTINENTAL Chêne-Bourg<br>
+										48, rue de Genève<br>
+										CH – 1225 Chêne-Bourg<br>
+										T +41 22 869 18 18<br>
+										<a href="mailto:chene@transcontinental.ch">chene@transcontinental.ch</a>
+									</p>
+								</div>
 							</div>
-						</a>
-					</div>
-					<?php if($i % 3 == 0){echo '</div><div class="row">'; } ?>
-					<?php $i++; ?>
-				<?php endforeach; ?>
-			</div>
-			<?php endif; ?>
-			<hr>
-			<br>
-
-			<h3>Contactez-nous</h3>
+							<p>Votre équipe Transcontinental</p>
 
 
-			<div class="row">
-				<div class="col-sm-4">
-					<p>
-						TRANSCONTINENTAL Florissant<br>
-						66, route de Florissant<br>
-						CH – 1206 Genève<br>
-						T +41 22 347 27 27<br>
-						<a href="mailto:info@transcontinental.ch">info@transcontinental.ch</a>
-					</p>
-				</div>
-				<div class="col-sm-4">
-					<p>
-						TRANSCONTINENTAL Chêne-Bourg<br>
-						48, rue de Genève<br>
-						CH – 1225 Chêne-Bourg<br>
-						T +41 22 869 18 18<br>
-						<a href="mailto:chene@transcontinental.ch">chene@transcontinental.ch</a>
-					</p>
-				</div>
-			</div>
-			<p>Votre équipe Transcontinental</p>
+							<br class="clear">
 
-
-					<br class="clear">
-
-					<?php edit_post_link(); ?>
+							<?php edit_post_link(); ?>
 
 
 
 
 
-				</article>
-				<!-- /article -->
+						</article>
+						<!-- /article -->
 
-			<?php endwhile; ?>
+					<?php endwhile; ?>
 
-		<?php else: ?>
+				<?php else : ?>
 
-			<!-- article -->
-			<article>
+					<!-- article -->
+					<article>
 
-				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+						<h2><?php _e('Sorry, nothing to display.', 'html5blank'); ?></h2>
 
-			</article>
-			<!-- /article -->
+					</article>
+					<!-- /article -->
 
-		<?php endif; ?>
+				<?php endif; ?>
 
+			</section>
+			<!-- /section -->
+
+
+			<?php //get_sidebar(); 
+			?>
+			<?php // get_template_part('sidebar_right'); 
+			?>
+
+
+		</div> <!-- END OF ROW -->
+	</div> <!-- END OF CONTAINER -->
+</div> <!-- END OF CONTAINER -->
+
+
+<?php if (get_the_title() == 'Contact') : ?>
+
+	<section style="margin: 0px 0 -20px;">
+		<div id="agencymap"></div>
 	</section>
-	<!-- /section -->
-
-
-	<?php //get_sidebar(); ?>
-	<?php // get_template_part('sidebar_right'); ?>
-
-
-</div> <!-- END OF ROW -->
-</div> <!-- END OF CONTAINER -->
-</div> <!-- END OF CONTAINER -->
-
-
-<?php if ( get_the_title() == 'Contact' ) : ?>
-
-<section style="margin: 0px 0 -20px;"><div id="agencymap"></div></section>
-<script type="text/javascript" src="//maps.google.com/maps/api/js?key=<?php google_maps_key(); ?>"></script>
 <?php endif; ?>
 
 
